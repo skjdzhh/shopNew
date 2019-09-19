f4bc70cad239247d14240cbf04ee233f3dacc4a2
kejunsun <kejunsun@linkdoc.com>
2019-09-19 21:24:20 +0800

    Home页面编写

--- README.md
+++ README.md
@@ -27,3 +27,45 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+<!-- 命名空间的命名法
+     public目录表示不用打包的资源文件
+     当在一个组件中不引入一个组件的时候，没有必要再这个组件中注册，
+     路由不算组件的引入，所以可以不用
+
+     view-router为自定义组件，
+
+     router-link,会被渲染成一个a标签
+      将本地项目上传到远端     
+     git:代码管理软件，版本控制工具
+      echo "# shopNew"     
+      git init
+      git add *         //将项目中所有的文件让git进行管理
+      git add README.md
+      git commit -m "first commit"
+      git remote add origin https://github.com/skjdzhh/shopNew.git
+      git push -u origin master
+
+      
+    
+
+      vant组件库（移动端）
+      推荐组件库：vux(移动端)
+      ElementUI(饿了吗)
+      iView（有生态，有后端管理系统，iChart）
+      i18n：国际化（view插件vue-i18n解决） 
+      muse-UI
+
+      安装插件
+      npm install xx --save             //生产环境
+      npm install xx --save-dev         //开发环境 
+
+      安装新的包后需要重启一下服务 
+
+      头部组件NavBar
+
+      热门商品  
+      npm install vue-awesome-swiper --save
+      npmjs
+
+    -->
--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 
     <router-view></router-view>
 
+  <!-- 标签栏，页面底部 -->      
     <van-tabbar v-model="active">
       <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
       <van-tabbar-item icon="records" to="category">分类</van-tabbar-item>
@@ -11,41 +12,7 @@
       <van-tabbar-item icon="contact" to="profile">我的</van-tabbar-item>
     </van-tabbar>
 
-    <!-- 命名空间的命名法
-     public目录表示不用打包的资源文件
-     当在一个组件中不引入一个组件的时候，没有必要再这个组件中注册，
-     路由不算组件的引入，所以可以不用
-
-     view-router为自定义组件，
-
-     router-link,会被渲染成一个a标签
-
-     git:代码管理软件，版本控制工具
-      echo "# shopNew" >> README.md
-      git init
-      git add *         //将项目中所有的文件让git进行管理
-      git add README.md
-      git commit -m "first commit"
-      git remote add origin https://github.com/skjdzhh/shopNew.git
-      git push -u origin master
-
-
-      vant组件库（移动端）
-      推荐组件库：vux(移动端)
-      ElementUI(饿了吗)
-      iView（有生态，有后端管理系统，iChart）
-      i18n：国际化（view插件vue-i18n解决）
-      muse-UI
-
-      安装插件
-      npm install xx --save             //生产环境
-      npm install xx --save-dev         //开发环境 
-
-      安装新的包后需要重启一下服务 
-
-      头部组件NavBar
-
-    -->
+  
   </div>
 </template>
 
